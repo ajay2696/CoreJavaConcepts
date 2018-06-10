0117a8d5b53f5f4dbc7593bb0cc1436cd93eddc3
Ajay Nara <33077370+ajay2696@users.noreply.github.com>
2018-06-10 13:47:11 -0400

    Check-in

---
+++ src/com/mine/practice/basic/BinaryOperatorTest.java
@@ -0,0 +1,12 @@
+package com.mine.practice.basic;
+
+public class BinaryOperatorTest {	
+	public static void main(String[] args) {
+		boolean b1=false;
+		boolean b2=true;
+		boolean b3=true;
+		
+		if(b1|b2&b1|b2)
+			System.out.println("first loop");
+	}
+}
